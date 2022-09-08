e6d3ce44ce056183c13da5221970456360552bf7
ManojTGN <manoj.thunderviz@gmail.com>
2022-09-09 02:17:39 +0530

    update sign-up

--- src/sign-up.php
+++ src/sign-up.php
@@ -1,3 +1,26 @@
+<?php  
+include("connection.php");
+?>
+
+<?php
+if(isset($_POST["createUser"])){
+    $userid = mysqli_fetch_array(mysqli_query($con,"SELECT MAX(`S NO`) from user;"))[0] + 1;
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    mysqli_query($con,"INSERT INTO `user`(`USER ID`, `USERNAME`, `PASSWORD`, `EMAIL`, `PHONE NUMBER`) VALUES ($userid,'$username','$password','$email',$phone)")or die(mysqli_error($con));
+
+    $_SESSION['userid']=$userid;
+    $_SESSION['username']=$username;
+    $_SESSION['email']=$email;
+    $_SESSION['phonenumber']=$phone;
+    $_SESSION['signed-in'] = true;
+
+    header("Location: index.php");
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,30 +33,30 @@
             <div class="card mx-auto" style="width: 30%;margin-top:10%;">
                 <div class="card-body">
                 <h5 class="card-title">Sign Up</h5><br>
-                <form>
+                <form method="POST">
                 <div class="input-group flex-nowrap" style="width: 300px;">
                     <span class="input-group-text" id="addon-wrapping">Username</span>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                    <input type="text" name="username" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" required>
                 </div>
                 <br>
                 <div class="input-group flex-nowrap" style="width: 300px;">
                     <span class="input-group-text" id="addon-wrapping">phone no</span>
-                    <input type="number" class="form-control" placeholder="phone no" aria-label="phone no" aria-describedby="addon-wrapping">
+                    <input type="number" name="phone" class="form-control" placeholder="phone no" aria-label="phone no" aria-describedby="addon-wrapping" required>
                 </div>
                 <br>
                 <div class="input-group flex-nowrap" style="width: 300px;">
                     <span class="input-group-text" id="addon-wrapping">Email ID</span>
-                    <input type="email" class="form-control" placeholder="Email" aria-label="mail" aria-describedby="addon-wrapping">
+                    <input type="email" name="email"class="form-control" placeholder="Email" aria-label="mail" aria-describedby="addon-wrapping" required>
                 </div>
                 <br>
                 <div class="input-group flex-nowrap" style="width: 300px;">
                     <span class="input-group-text" id="addon-wrapping" >password</span>
-                    <input type="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="addon-wrapping">
+                    <input type="password" name="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="addon-wrapping" required>
                 </div>
                 <br>
                 <br>
                 <div style="text-align: left;" style="width: 200px;">
-                    <button type="submit" class="btn btn-primary">Sign up</button>
+                    <button type="submit" name="createUser" class="btn btn-primary">Sign up</button>
                 </div>
                 </form>    
             
