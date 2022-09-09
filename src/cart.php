50cad2e989f3dfec9805b027e8d418a973216744
ManojTGN <manoj.thunderviz@gmail.com>
2022-09-09 15:50:05 +0530

    add order

--- src/cart.php
+++ src/cart.php
@@ -10,6 +10,15 @@ $CartCount = mysqli_num_rows($query);
 ?>
 
 <?php
+if(isset($_POST["proceed"])){
+    if($CartCount > 0){
+        $cartid = mysqli_fetch_array(mysqli_query($con,"SELECT `CART_ID` from cart WHERE`USER ID`=$userid;"))[0];
+        mysqli_query($con,"UPDATE cart SET `ORDERED`=1 WHERE`USER ID`=$userid;");
+        mysqli_query($con,"INSERT INTO `order`(`USER_ID`, `CART_ID`, `NOW`) VALUES ($userid,$cartid,1)")or die(mysqli_error($con));
+        header("Location: order.php");
+    }
+}
+
 if(isset($_POST["decreaseQuantity"])){
     $dishid = $_POST["dishid"];
     $cartid = $_POST["cartid"];
@@ -69,12 +78,9 @@ if(isset($_POST["removeItem"])){
                             $query = mysqli_query($con,"select * from cart where `USER ID`='$userid' AND `ORDERED`=false")or die(mysqli_error($con));
                             while($cart=mysqli_fetch_array($query)){
                                 $dish_id = $cart['DISH_ID'];
-                                $rest_id = $cart['REST_ID'];
-
                                 $dish_price = $cart['PRICE'];
                                 $dish_quantity = $cart['QUANTITY'];
                                 $dish_name = mysqli_fetch_array(mysqli_query($con,"select `DISH_NAME` from dishes WHERE `DISH_ID`=$dish_id"))[0];
-                                $rest_name = mysqli_fetch_array(mysqli_query($con,"select `REST_NAME` from restaurant WHERE `REST_ID`=$rest_id"))[0];
                         ?><br>
                         <div class="card w-100">
                             <div class="card-body">
@@ -92,13 +98,11 @@ if(isset($_POST["removeItem"])){
                                         <div class="row">
                                             <div class="col">
                                                 <b>Dish:</b><br>
-                                                <b>Restaurant:</b><br>
                                                 <b>Price:</b><br>
                                                 <b>Quantity:</b>
                                             </div>
                                             <div class="col">
                                                 <?php echo $dish_name;?><br>
-                                                <?php echo $rest_name;?><br>
                                                 <?php echo $dish_price;?><br>
                                                 <ul class="pagination pagination-sm">
                                                     <li class="page-item"><button class="page-link" name="decreaseQuantity">-</button></li>
@@ -117,9 +121,21 @@ if(isset($_POST["removeItem"])){
                         <br><br><br>
                     </div>
                     <div class="col-4">
-                        <br><br><br>
                         <div class="row">
                             <div class="col-8">
+                                <div class="row">
+                                    <div class="col"><h5 class="card-subtitle mb-2">Dish</h5></div>
+                                    <div class="col"><h5 class="card-subtitle mb-2">Quantity</h5></div>
+                                </div>
+                                <div style="width:100%;border:1px solid black;border-style:dashed;"></div><br>
+                                <?php 
+                                    if($CartCount <= 0){
+                                ?>
+                                <div class="row">
+                                    <div class="col"><h6 class="card-subtitle mb-2 text-center"><i class="fa-solid fa-cart-shopping text-muted" style="font-size:30px;"></i></h6></div>
+                                    <div class="col"><h6 class="card-subtitle mb-2 text-muted">Empty Cart!</h6></div>
+                                </div>
+                                <?php }?>
                                 <?php
                                     $total_price = 0;
                                     $query = mysqli_query($con,"select * from cart where `USER ID`='$userid' AND `ORDERED`=false")or die(mysqli_error($con));
@@ -135,6 +151,13 @@ if(isset($_POST["removeItem"])){
                                 <?php }?>
                             </div>
                             <div class="col-4" style="text-align:right;">
+                                <h5 class="card-subtitle mb-2">Price</h5>
+                                <div style="width:100%;border:1px solid black;border-style:dashed;"></div><br>
+                                <?php 
+                                    if($CartCount <= 0){
+                                ?>
+                                <h6 class="card-subtitle mb-2">0</h6>
+                                <?php }?>
                                 <?php
                                     $query = mysqli_query($con,"select * from cart where `USER ID`='$userid' AND `ORDERED`=false")or die(mysqli_error($con));
                                     while($cart=mysqli_fetch_array($query)){
@@ -145,8 +168,8 @@ if(isset($_POST["removeItem"])){
                                 <h6 class="card-subtitle mb-2"><?php echo $dish_price*$dish_quantity;?></h6>
                                 <?php }?>
                             </div>
-                        </div><br>
-                        <div style="width:100%;border:1px solid black;border-style:dashed;"></div><br>
+                        </div>
+                        <br><div style="width:100%;border:1px solid black;border-style:dashed;"></div><br>
                         <div class="row">
                             <div class="col-8">
                                 <h6 class="card-subtitle mb-2">Total:</h6>
@@ -155,8 +178,9 @@ if(isset($_POST["removeItem"])){
                                 <h6 class="card-subtitle mb-2"><?php echo $total_price;?></h6>
                             </div>
                         </div><br>
-                        
-                        <button class="btn btn-primary w-100">Proceed</button>
+                        <form method="POST">
+                        <button class="btn btn-primary w-100" name="proceed">Proceed</button>
+                        </form>
                     </div>
                 </div>
                 
---
+++ src/order.php
@@ -0,0 +1,122 @@
+<?php  
+include("connection.php");
+if(!isset($_SESSION['signed-in'])){header("Location: sign-in.php");}
+?>
+
+<?php
+$userid = $_SESSION['userid'];
+$query = mysqli_query($con,"select `USER_ID` from `order` where `USER_ID`=$userid;")or die(mysqli_error($con));
+$OrderCount = mysqli_num_rows($query);
+?>
+
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/08c3f952c9.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            
+            <div class="card mx-auto" style="width:50%;margin-top:20px;">
+                <div class="card-body">
+                    <h5 class="card-title">Your Orders</h5><hr>
+                    <?php if($OrderCount <= 0){?>
+                        <h3 class="text-muted">You Havent Ordered Anything yet</h3>
+                    <?php 
+                    }else{
+                    $query = mysqli_query($con,"select * from `order` where `USER_ID`='$userid'")or die(mysqli_error($con));
+                    while($order=mysqli_fetch_array($query)){
+                        $orderid = $order["ORDER_ID"];
+                        $track = $order["TRACK"];
+                        $now = $order["NOW"];
+
+                        if($track == 0){
+                            $track = 0 + $now;
+                        }else if($track == 1){
+                            $track = 50 + $now;
+                        }else if($track == 2){
+                            $track = 100;
+                        }else{
+                            $track = 0;
+                        }
+                    ?>
+                    <div class="card">
+                        <div class="card-header" <?php if($track >= 100){?>style="color:green;"<?php }?>> <b><?php if($track >= 100){?><i class="fa-solid fa-circle-check"></i><?php }?>Order (<code><?php echo $orderid;?></code>)</b></div>
+                        <div class="card-body">
+                        <div class="accordion" id="accordionPanelsStayOpenExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="panelsStayOpen-trackOrder">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">Track Order</button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-trackOrder">
+                                <div class="accordion-body">
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" aria-label="Default striped example" style="width: <?php echo $track;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col text-start" <?php if($track > 0){?>style="color:green;"<?php }?>><b><?php if($track > 0){?><i class="fa-solid fa-circle-check"></i><?php }?>Order</b></div>
+                                        <div class="col text-center"<?php if($track >= 50){?>style="color:green;"<?php }?>><b><?php if($track >= 50){?><i class="fa-solid fa-circle-check"></i><?php }?>Restaurant</b></div>
+                                        <div class="col text-end"  <?php if($track >= 100){?>style="color:green;"<?php }?>><b><?php if($track >= 100){?><i class="fa-solid fa-circle-check"></i><?php }?>Delivered</b></div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="panelsStayOpen-allDishes">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">Order</button>
+                                </h2>
+                                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-allDishes">
+                                <div class="accordion-body">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Dish</th>
+                                            <th scope="col">Quantity</th>
+                                            <th scope="col">Price</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php 
+                                                $index = 1;
+                                                $totalprice = 0;
+                                                $cartid = mysqli_fetch_array(mysqli_query($con,"select `CART_ID` from `order` where `USER_ID`='$userid'"))[0];
+                                                $query = mysqli_query($con,"SELECT * FROM `cart` WHERE `USER ID`='$userid' AND `ORDERED`=1 AND `CART_ID`=$cartid")or die(mysqli_error($con));
+                                                while($cart=mysqli_fetch_array($query)){
+                                                    $dishid = $cart["DISH_ID"];
+                                                    $dishquantity = $cart["QUANTITY"];
+                                                    $dishprice = $cart["PRICE"] * $dishquantity;
+                                                    $dishname = mysqli_fetch_array(mysqli_query($con,"select `DISH_NAME` from dishes WHERE `DISH_ID`=$dishid"))[0];
+                                                    $totalprice += $dishprice;
+                                            ?>
+                                            <tr>
+                                            <th scope="row"><?php echo $index;$index++;?></th>
+                                            <td><?php echo $dishname;?></td>
+                                            <td><?php echo "x".$dishquantity;?></td>
+                                            <td><?php echo $dishprice;?></td>
+                                            </tr>
+                                            <?php }?>
+                                            <tr>
+                                            <th scope="row"></th>
+                                            <td></td>
+                                            <td style="text-end"><b>Total</b></td>
+                                            <td><b><?php echo $totalprice;?></b></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    </div>
+                                </div>
+                                </div>
+                            </div><br>
+                        <button class="btn btn-danger">Cancel Order</button>
+                        <button class="btn btn-link">Delivered (beta)</button>
+                        </div>
+                    </div>
+                    <?php }}?>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
