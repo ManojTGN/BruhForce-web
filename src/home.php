7f54fd2a6d90e84fda8199b66ee234f244d9b541
ManojTGN <manoj.thunderviz@gmail.com>
2022-09-10 08:23:58 +0530

    changes in homr

---
+++ src/home.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    </head>
+    <body>
+        <div style="margin:0;padding:0;" class="container-fluid">
+        <div style="margin:0" class="row w-100">
+            <div class="col-4">col-4</div>
+            <div style="background-color:blue;height:2000px;"class="col-8">
+                    <form class="form-inline my-2 my-lg-0 d-flex">
+                        <input style="width: 60%; height: 30%;"class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button style="height: 30%;"class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        <div class="centered">
+                    <nav class="nav nav-pills flex-column flex-sm-row">
+                        <a class="flex-sm-fill text-sm-center nav-link" href="#">Home</a>
+                        <a class="flex-sm-fill text-sm-center nav-link" href="#">Cart</a>
+                        <a class="flex-sm-fill text-sm-center nav-link" href="#">User</a>
+                </nav>
+                </div>
+                </form>
+               
+                <div style="height: 25%;display:flex;flex-wrap: nowrap;flex-direction: row;" class="card">
+                    <div style="height: 30%;background:black; width: 30%;margin-right:10px;" class="card"></div>
+                    <div style="height: 30%;background:black; width: 30%;margin-right:10px;" class="card"></div>
+                    <div style="height: 30%;background:black; width: 30%;margin-right:10px;" class="card"></div>
+                    <div style="height: 30%;background:black; width: 30%;margin-right:10px;" class="card"></div>
+                    <div style="height: 30%;background:black; width: 30%;margin-right:10px;" class="card"></div>
+                </div>
+                
+                
+            </div>
+            </div>
+        </div>
+        </div>
+    </body>
